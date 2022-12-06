78390e7fc3a76fbb5ad88254da39bb1369f72939
Nagaprabhanjan Bellari <nagaprabhanjan@gmail.com>
2022-12-06 19:27:47 +0530

    Add hackerRank python code

---
+++ python/combinations.py
@@ -0,0 +1,14 @@
+import itertools
+
+nElems = int(input())
+elems = input().split()
+k = int(input())
+
+# find the probabality of seeing 'a' when k elements are chosen randomly from the list
+nCk = list(itertools.combinations(elems, k))
+nOccurances = 0
+for combo in nCk:
+    if 'a' in combo:
+        nOccurances += 1
+
+print("%.3f" % ((1.0 * nOccurances)/len(nCk)))
